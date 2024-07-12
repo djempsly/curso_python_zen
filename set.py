8d3eddbd049150df63199bd65ed62e92a7da2be2
Jeffry Almonte Dely <djempsly120@gmail.com>
2024-07-12 11:51:38 -0400

    TRabajando con los Sets

---
+++ set.py
@@ -0,0 +1,25 @@
+# Esta clase es para aprender acerca de los sets en Python.
+
+set_countries = {'Haiti', 'R.D.', 'Venezuela'}
+print(set_countries)
+print(type(set_countries))
+
+# Los sets no aceptan o no imprimen valores dobles
+set_numbers = {1, 2, 3, 2, 4, 5, }
+print(set_numbers)
+
+nombre = 'Juan Antonio'
+print(nombre)
+nombre_2 = nombre.replace('Juan', 'Jeffry')
+print(nombre_2)
+
+
+tupla_nombre = ('Maline', 'Kiki', 'Rodelyn', 'Maline', 'Rosecamelle')
+set_tupla = set(tupla_nombre)
+print(set_tupla)
+
+list_1 = ['Jeffry', 1, False, True, 'Volmar']
+set_list = set(list_1)
+print(set_list)
+
+
